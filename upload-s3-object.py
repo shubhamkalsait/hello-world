cb2a6e9bf6cc4c65090752cbbfddd8c85dfb239c
unknown <ash.shubham.kalsait@gmail.com>
2020-05-11 18:58:40 +0530

    adding s3 upload object lines

--- upload-s3-object.py
+++ upload-s3-object.py
@@ -1,1 +1,9 @@
 import boto3
+from botocore.client import Config
+s3 = boto3.resource('s3')
+
+data =  open('hello.csv','rb')
+bucket_name = 'cbz-b15'
+
+s3.Bucket(bucket_name).put_object(Key='test.py', Body=data)
+print('done')
