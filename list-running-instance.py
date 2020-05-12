28462403cdd6adcbde7741e553b862bfd3f675bb
unknown <ash.shubham.kalsait@gmail.com>
2020-05-12 09:29:40 +0530

    search all region and print running instance's id

--- list-running-instance.py
+++ list-running-instance.py
@@ -2,15 +2,19 @@ import boto3
 
 client = boto3.client('ec2')
 
-#Fetching instances information with adding filter for running instances
-all_data_temp = client.describe_instances(Filters=[{'Name':'instance-state-name','Values':['running']}])
+#getting all regions 
+regions = client.describe_regions()['Regions']
+for region in regions:
+    get_reg_name = region['RegionName']
 
-#getting instance id from all data fetched earlier
-#reservation_temp stores list of instance_info
-reservation_temp = all_data_temp['Reservations']
-for i in range(len(reservation_temp)):
-    instance_id = reservation_temp[i]['Instances'][0]['InstanceId']
-    print(instance_id)
+#creating client object for each region
+    client = boto3.client('ec2',region_name = get_reg_name)
 
+#getting running instances
+    instances_info = client.describe_instances(Filters=[{'Name':'instance-state-name','Values':['running']}])['Reservations']
 
-
+#printing runing instance's id
+    for instance in instances_info:
+        instance_id = instance['Instances'][0]['InstanceId']
+        print("Region:  ",get_reg_name,end="  |  ")
+        print('Instance Id: ',instance_id)
