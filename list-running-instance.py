d51a3ff598ac36a46c39ca6c6eb13a7f5fbb9c72
unknown <ash.shubham.kalsait@gmail.com>
2020-05-12 08:44:22 +0530

    shortening the logic

--- list-running-instance.py
+++ list-running-instance.py
@@ -1,19 +1,16 @@
-import boto3        #python library for aws
+import boto3
 
-ec2_obj = boto3.client('ec2')           #contain describe_instances() method
-ec2_obj1 = boto3.resource('ec2')        #contain Instance() method 
-result = ec2_obj.describe_instances() 
-reservation = result['Reservations']    #describe_instance() returns big nested dictionary
-                                        #we search dictionary for desired output
+client = boto3.client('ec2')
 
-for i in range(len(reservation)):
-    temp = reservation[i]
-    instances = temp['Instances']
-    temp = instances[0]
-    instance_id = temp['InstanceId']
-    instance = ec2_obj1.Instance(instance_id)
-    temp_instance_state = instance.state
-    if temp_instance_state['Name']=='running':
-        print('Instance ID {} : {}'.format(i+1,instance_id), end="    ")
-        print('Availability Zone : {}'.format(temp['Placement']['AvailabilityZone']))
+#Fetching instances information with adding filter for running instances
+all_data_temp = client.describe_instances(Filters=[{'Name':'instance-state-name','Values':['running']}])
+
+#getting instance id from all data fetched earlier
+#reservation_temp stores list of instance_info
+reservation_temp = all_data_temp['Reservations']
+for i in range(len(reservation_temp)):
+    instance_id = reservation_temp[i]['Instances'][0]['InstanceId']
+    print(instance_id)
+
+
 
